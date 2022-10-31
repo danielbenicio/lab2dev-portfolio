66ae1aa8febb43f08c742de35fcacdc7f11a1ec8
Daniel <danielbenicio123@gmai.com>
2022-10-31 16:28:44 -0300

    fix(project-information): remove hook dont use

--- src/pages/project-information/[id].tsx
+++ src/pages/project-information/[id].tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import { GetStaticPaths, GetStaticProps } from 'next'
 import { v4 as uuidv4 } from 'uuid'
 import Modal from 'react-modal'
